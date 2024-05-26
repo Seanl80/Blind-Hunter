3a2a589dd1a8fe8f32442a44013e62963f6513ce
Sean Littlewood <seanlitt80@gmail.com>
2024-05-26 13:21:23 +0000

    nav links added

--- blindshunter/routes.py
+++ blindshunter/routes.py
@@ -6,3 +6,13 @@ from blindshunter.models import Company, Review
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
+@app.route("/companies")
+def companies():
+    return render_template("companies.html")
+
+
+@app.route("/reviews")
+def reviews():
+    return render_template("reviews.html")
