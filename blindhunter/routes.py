6e722b58f32d403b24ad36483575f24f2b6bdb79
Sean Littlewood <seanlitt80@gmail.com>
2024-05-26 18:56:37 +0000

    add company route

--- blindhunter/routes.py
+++ blindhunter/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from blindhunter import app, db
 from blindhunter.models import Company, Review
 
@@ -11,6 +11,20 @@ def home():
 @app.route("/companies")
 def companies():
     return render_template("companies.html")
+
+
+@app.route("/add_company", methods=["GET", "POST"])
+def add_company():
+    if request.method == "POST":
+        company = Company(
+            company_name=request.form.get("company_name"),
+            blinds_sold=request.form.get("blinds_sold"),
+            contact_number=request.form.get("contact_number"),
+            uk_area=request.form.get("uk_area"))
+        db.session.add(company)
+        db.session.commit()
+        return redirect(url_for("companies"))
+    return render_template("add_company.html")
 
 
 @app.route("/reviews")
