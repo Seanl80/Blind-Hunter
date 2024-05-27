249c88f3b377a3c684467bde01d0e058041291a4
Sean Littlewood <seanlitt80@gmail.com>
2024-05-27 19:11:26 +0000

    reviews added

--- blindhunter/routes.py
+++ blindhunter/routes.py
@@ -1,11 +1,13 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, session
 from blindhunter import app, db
-from blindhunter.models import Company, Detail
+from blindhunter.models import Company, Detail, Review
+
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("home.html")
 
 
 @app.route("/companies")
@@ -44,4 +46,33 @@ def delete_company(company_id):
 
 @app.route("/reviews")
 def reviews():
-    return render_template("reviews.html")
+    reviews = list(Review.query.order_by(Review.review_name).all())
+    return render_template("reviews.html", reviews=reviews)
+
+
+@app.route("/add_review", methods=["GET", "POST"])
+def add_review():
+    if request.method == "POST":
+        review = Review(review_name=request.form.get("review_name"))
+        db.session.add(review)
+        db.session.commit()
+        return redirect(url_for("reviews"))
+    return render_template("add_review.html")
+
+
+@app.route('/edit_review/<int:review_id>', methods=['GET', 'POST'])
+def edit_review(review_id):
+    review = Review.query.get_or_404(review_id)
+    if request.method == 'POST':
+        review.content = request.form['content']
+        db.session.commit()
+        return redirect('reviews')
+    return render_template('edit_review.html', review=review)
+
+
+@app.route('/delete_review/<int:review_id>', methods=['POST'])
+def delete_review(review_id):
+    review = Review.query.get_or_404(review_id)
+    db.session.delete(review)
+    db.session.commit()
+    return redirect('reviews')
