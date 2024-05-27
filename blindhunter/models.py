ebf6972c3baca2790ccb0099adb106af0c659ffc
Sean Littlewood <seanlitt80@gmail.com>
2024-05-27 17:57:45 +0000

    reviews added

--- blindhunter/models.py
+++ blindhunter/models.py
@@ -24,15 +24,15 @@ class Detail(db.Model):
          )
 
 
+class Review(db.Model):
+    # schema for the Review model
+    id = db.Column(db.Integer, primary_key=True)
+    review_name = db.Column(db.String(25), unique=True, nullable=False)
+    review_date = db.Column(db.Date, nullable=False)
+    review_description = db.Column(db.Text, nullable=False)
+    company_id = db.Column(db.Integer, db.ForeignKey("company.id", ondelete="CASCADE"), nullable=False)
 
-# class Review(db.Model):
-#     # schema for the Review model
-#     id = db.Column(db.Integer, primary_key=True)
-#     review_name = db.Column(db.String(25), unique=True, nullable=False)
-#     review_date = db.Column(db.Date, nullable=False)
-#     review_description = db.Column(db.Text, nullable=False)
-#     company_id = db.Column(db.Integer, db.ForeignKey("company.id", ondelete="CASCADE"), nullable=False)
-
-#     def __repr__(self):
-#         return self.review_name
-
+    def __repr__(self):
+         return "#{0} - Detail: {1} | Contact: {2}".format(
+            self.id, self.review_name, self.review_date
+         )
