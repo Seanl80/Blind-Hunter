3b0b2a2c37ecd0d54b137beb2804a4736cfb7364
Sean Littlewood <seanlitt80@gmail.com>
2024-05-27 13:17:10 +0000

    company route split to details

--- blindhunter/models.py
+++ blindhunter/models.py
@@ -5,22 +5,34 @@ class Company(db.Model):
     # schema for the Company model
     id = db.Column(db.Integer, primary_key=True)
     company_name = db.Column(db.String(50), unique=True, nullable=False)
-    blinds_sold = db.Column(db.Text, nullable=False)
-    contact_number = db.Column(db.Integer, nullable=False)
-    uk_area = db.Column(db.String(25), unique=True, nullable=False)
-    reviews = db.relationship("Review", backref="company", cascade="all, delete", lazy=True)
+    details = db.relationship("Detail", backref="company", cascade="all, delete", lazy=True)
 
     def __repr__(self):
         return self.company_name
 
-class Review(db.Model):
-    # schema for the Review model
+class Detail(db.Model):
+    # schema for the Details model
     id = db.Column(db.Integer, primary_key=True)
-    review_name = db.Column(db.String(25), unique=True, nullable=False)
-    review_date = db.Column(db.Date, nullable=False)
-    review_description = db.Column(db.Text, nullable=False)
+    detail_area = db.Column(db.String(25), unique=True, nullable=False)
+    detail_description = db.Column(db.Text, nullable=False)
+    detail_contact = db.Column(db.Integer, nullable=False)
     company_id = db.Column(db.Integer, db.ForeignKey("company.id", ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
-        return self.review_name
+         return "#{0} - Detail: {1} | Contact: {2}".format(
+            self.id, self.detail_area, self.detail_contact
+         )
+
+
+
+# class Review(db.Model):
+#     # schema for the Review model
+#     id = db.Column(db.Integer, primary_key=True)
+#     review_name = db.Column(db.String(25), unique=True, nullable=False)
+#     review_date = db.Column(db.Date, nullable=False)
+#     review_description = db.Column(db.Text, nullable=False)
+#     company_id = db.Column(db.Integer, db.ForeignKey("company.id", ondelete="CASCADE"), nullable=False)
+
+#     def __repr__(self):
+#         return self.review_name
 
--- blindhunter/routes.py
+++ blindhunter/routes.py
@@ -1,26 +1,23 @@
 from flask import render_template, request, redirect, url_for
 from blindhunter import app, db
-from blindhunter.models import Company, Review
+from blindhunter.models import Company, Detail
 
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("base.html")
 
 
 @app.route("/companies")
 def companies():
-    return render_template("companies.html")
+    companies = list(Company.query.order_by(Company.company_name).all())
+    return render_template("companies.html", companies=companies)
 
 
 @app.route("/add_company", methods=["GET", "POST"])
 def add_company():
     if request.method == "POST":
-        company = Company(
-            company_name=request.form.get("company_name"),
-            blinds_sold=request.form.get("blinds_sold"),
-            contact_number=request.form.get("contact_number"),
-            uk_area=request.form.get("uk_area"))
+        company = Company(company_name=request.form.get("company_name"))
         db.session.add(company)
         db.session.commit()
         return redirect(url_for("companies"))
