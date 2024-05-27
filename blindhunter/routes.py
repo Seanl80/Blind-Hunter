feb7f11b354770ac7863efef9fadba2e032616c5
Sean Littlewood <seanlitt80@gmail.com>
2024-05-27 14:13:06 +0000

    edit companies added in

--- blindhunter/routes.py
+++ blindhunter/routes.py
@@ -24,6 +24,16 @@ def add_company():
     return render_template("add_company.html")
 
 
+@app.route("/edit_company/<int:company_id>", methods=["GET", "POST"])
+def edit_company(company_id):
+    company = Company.query.get_or_404(company_id)
+    if request.method == "POST":
+        company.company_name = request.form.get("company_name")
+        db.session.commit()
+        return redirect(url_for("companies"))
+    return render_template("edit_company.html", company=company)
+
+
 @app.route("/reviews")
 def reviews():
     return render_template("reviews.html")
