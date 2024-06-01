f6417ea49768a9545455ee91994b23718e905e96
Sean Littlewood <seanlitt80@gmail.com>
2024-06-01 12:35:58 +0000

    datepicker added

--- blindhunter/static/js/script.js
+++ blindhunter/static/js/script.js
@@ -1,10 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-  });
+
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "d m, yy",
+      i18n: {done: "Select"}
+    });
+});
 
 document.addEventListener('DOMContentLoaded', function() {
     let modal = document.querySelectorAll('.modal');
     M.Modal.init(modal);
   });
-
