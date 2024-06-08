e3c790099e5e1a9bd50dfb5fee85b2f74b6b4902
Sean Littlewood <seanlitt80@gmail.com>
2024-06-08 10:54:56 +0000

    datepicker removed for simpler code for preselecting todays date

--- blindhunter/static/js/script.js
+++ blindhunter/static/js/script.js
@@ -2,18 +2,22 @@
 document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-// datepicker initialization
-    let datepicker = document.querySelectorAll('.datepicker');
-    M.Datepicker.init(datepicker, {
-      format: "dd/mm/yy",
-      i18n: {done: "Select"}
-    });
+});
+
+// date initialization
+document.addEventListener('DOMContentLoaded', function() {
+    let today = new Date().toISOString().substring(0, 10);
+    document.getElementById('date').value = today;
+});
+
 // select company in review initialization
+document.addEventListener('DOMContentLoaded', function() {
     let selects = document.querySelectorAll('select');
     M.FormSelect.init(selects);
 });
+
 // modal initialization
 document.addEventListener('DOMContentLoaded', function() {
     let modal = document.querySelectorAll('.modal');
     M.Modal.init(modal);
-  });
+});
